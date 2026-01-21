042ea2bc4da03ff71b1590aad4953287bfa204c4
Diego Rapichan <direrapichan@gmail.com>
2026-01-20 21:27:25 -0300

    Format BoardColumnKindEnum.java for consistency

--- src/main/java/br/com/dio/persistence/entity/BoardColumnKindEnum.java
+++ src/main/java/br/com/dio/persistence/entity/BoardColumnKindEnum.java
@@ -3,7 +3,7 @@ package br.com.dio.persistence.entity;
 import java.util.stream.Stream;
 
 public enum BoardColumnKindEnum {
-
+ 
     INITIAL, FINAL, CANCEL, PENDING;
 
     public static BoardColumnKindEnum findByName(final String name){
