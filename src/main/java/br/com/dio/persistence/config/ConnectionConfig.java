7268f38f97472171bb52fff9222ebbc13fe47b52
Diego Rapichan <direrapichan@gmail.com>
2026-01-20 21:25:33 -0300

    Remove unnecessary blank line in ConnectionConfig

--- src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+ 
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
